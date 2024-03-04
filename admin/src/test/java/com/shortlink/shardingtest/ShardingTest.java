02bdb6b9d598fa5dd59601ee39c1aa8a79e5499c
a12bb <2713204748@qq.com>
2024-03-04 17:13:06 +0800

    短链接分表

--- admin/src/test/java/com/shortlink/shardingtest/ShardingTest.java
+++ admin/src/test/java/com/shortlink/shardingtest/ShardingTest.java
@@ -7,20 +7,26 @@ package com.shortlink.shardingtest;
 
 
 public class ShardingTest {
-    private static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+    private static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
+            "  `click_num` int DEFAULT '0' COMMENT '点击量',\n" +
+            "  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',\n" +
+            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：启用 1：未启用',\n" +
+            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：接口创建 1：控制台创建',\n" +
+            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：自定义',\n" +
+            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_time` bigint DEFAULT '0' COMMENT '删除时间戳',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1763075463445028867 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+            "  UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`,`del_time`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1764576595958919170 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++){
