ec29fbf8e23c4694b145157a700d7eb229985780
a12bb <2713204748@qq.com>
2024-03-07 18:17:54 +0800

    修改文件名

--- admin/src/main/java/com/shortlink/remote/dto/ShortLinkRemoteService.java
+++ admin/src/main/java/com/shortlink/remote/dto/ShortLinkRemoteService.java
@@ -113,7 +113,7 @@ public interface ShortLinkRemoteService {
      * @param requestParam 请求参数
      * @return void
      */
-    default void recoverShortLinkRecycleBin(RecycleBinRecoverDTO requestParam){
+    default void recoverShortLinkRecycleBin(RecycleBinRecoverReqDTO requestParam){
         HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/recover", JSON.toJSONString(requestParam));
     }
 }
